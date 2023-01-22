d7a765727a081646b6bb8ec6451df64ac02c073e
eslam <eslamelbazedy@gmail.com>
2023-01-22 19:29:34 +0200

    add form

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,7 +47,54 @@
         </header>
 
     <main class="container mx-auto max-w-custom flex">
-        <div class="w-70 mr-5 line-clamp-3">add idea Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta magnam nemo neque nobis sunt! Amet, beatae delectus distinctio eveniet exercitationem expedita libero odit officiis perferendis praesentium quos rerum similique voluptatem.</div>
+        <div class="w-70 mr-5">
+            <div class="bg-white border-2 border-blue rounded-xl mt-16"
+            style="
+            border-image-source: linear-gradient(to bottom, rgba(50,138,241,0.22), rgba(99,123,255,0));
+            border-image-slice: 1;
+            background-image: linear-gradient(to bottom, #ffffff, #ffffff), linear-gradient(to bottom, rgba(50,138,241,0.22), rgba(99,123,255,0));
+            background-origin: border-box;
+            background-clip: content-box, border-box;
+            ">
+                <div class="text-center px-6 py-2 pt-6">
+                    <h3 class="font-semibold text-base">Add an idea</h3>
+                    <p class="text-xs mt-4">Let us know what you would like and we'll take a look over!</p>
+                </div>
+                <form action="#" method="post" class="space-y-4 px-4 py-6">
+                    <div>
+                        <input type="text" class="w-full text-sm bg-gray-100 rounded-xl border-none placeholder-gray-900 px-4 py-2" placeholder="Your Idea">
+                    </div>
+                    <div>
+                        <select name="category_add" id="category_add" class="w-full bg-gray-100 text-sm rounded-xl border-none px-4 py-2">
+                            <option value="category one">Category One</option>
+                            <option value="category two">Category Two</option>
+                            <option value="category three">Category Three</option>
+                        </select>
+                    </div>
+                    <div>
+                        <textarea name="idea" id="idea" cols="30" rows="4" class="w-full text-sm bg-gray-100 rounded-xl border-none placeholder-gray-900 px-4 py-2"
+                        placeholder="Describe your idea"></textarea>
+                    </div>
+
+                    <div class="flex items-center justify-between space-x-3">
+                        <button type="button"
+                        class="flex items-center justify-center w-1/2 h-11 text-xs bg-gray-200 font-semibold rounded-xl border border-gray-200 hover:border-gray-400 transition duration-150 ease-in px-6 py-3"
+                        >
+                            <svg class="text-gray-600 w-5 -rotate-45" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M18.375 12.739l-7.693 7.693a4.5 4.5 0 01-6.364-6.364l10.94-10.94A3 3 0 1119.5 7.372L8.552 18.32m.009-.01l-.01.01m5.699-9.941l-7.81 7.81a1.5 1.5 0 002.112 2.13" />
+                            </svg>
+                            <span class="ml-1"> Attach</span>
+                        </button>
+                        <button type="submit"
+                        class="flex items-center justify-center text-white w-1/2 h-11 text-xs bg-blue font-semibold rounded-xl border border-blue hover:bg-blue-hover transition duration-150 ease-in px-6 py-3"
+                        >
+
+                            <span class="ml-1"> Submit</span>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
         <div class="w-175">
             <nav class="flex items-center justify-between text-xs">
                 <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
@@ -65,6 +112,8 @@
             <div class="mt-8">
                 {{$slot}}
             </div>
+        </div>
+        <div class="w-24">
         </div>
     </main>
     </body>
